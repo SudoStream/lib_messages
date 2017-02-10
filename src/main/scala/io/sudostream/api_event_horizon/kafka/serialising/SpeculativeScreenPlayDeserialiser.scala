0489fb68197b953e850a33185df77ec9c6a05a7e
Andy Boyle <andy@sudostream.io>
2017-02-10 11:28:13 +0000

    Rename GeneratedTestsEvent to SpeculativeScreenPlay

--- src/main/scala/io/sudostream/api_event_horizon/kafka/serialising/SpeculativeScreenPlayDeserialiser.scala
+++ src/main/scala/io/sudostream/api_event_horizon/kafka/serialising/SpeculativeScreenPlayDeserialiser.scala
@@ -3,19 +3,19 @@ package io.sudostream.api_event_horizon.kafka.serialising
 import java.io.{ByteArrayInputStream, InputStream}
 import java.util
 
-import io.sudostream.api_event_horizon.messages.GeneratedTestsEvent
+import io.sudostream.api_event_horizon.messages.SpeculativeScreenPlay
 import org.apache.avro.io.DecoderFactory
 import org.apache.avro.specific.SpecificDatumReader
 import org.apache.kafka.common.serialization.Deserializer
 
-class GeneratedTestsEventDeserialiser extends Deserializer[GeneratedTestsEvent] {
+class SpeculativeScreenPlayDeserialiser extends Deserializer[SpeculativeScreenPlay] {
   override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {}
 
-  override def deserialize(topic: String, data: Array[Byte]): GeneratedTestsEvent = {
-    val reader = new SpecificDatumReader[GeneratedTestsEvent](GeneratedTestsEvent.SCHEMA$)
+  override def deserialize(topic: String, data: Array[Byte]): SpeculativeScreenPlay = {
+    val reader = new SpecificDatumReader[SpeculativeScreenPlay](SpeculativeScreenPlay.SCHEMA$)
     val in: InputStream = new ByteArrayInputStream(data)
     val decoder: org.apache.avro.io.Decoder = new DecoderFactory().binaryDecoder(in, null)
-    val outVersion = new GeneratedTestsEvent()
+    val outVersion = new SpeculativeScreenPlay()
     reader.read(outVersion, decoder)
     outVersion
   }
--- src/main/scala/io/sudostream/api_event_horizon/kafka/serialising/SpeculativeScreenPlaySerializer.scala
+++ src/main/scala/io/sudostream/api_event_horizon/kafka/serialising/SpeculativeScreenPlaySerializer.scala
@@ -3,17 +3,17 @@ package io.sudostream.api_event_horizon.kafka.serialising
 import java.io.ByteArrayOutputStream
 import java.util
 
-import io.sudostream.api_event_horizon.messages.GeneratedTestsEvent
+import io.sudostream.api_event_horizon.messages.SpeculativeScreenPlay
 import org.apache.avro.io.{DatumWriter, EncoderFactory}
 import org.apache.avro.specific.SpecificDatumWriter
 import org.apache.kafka.common.serialization.Serializer
 
-class GeneratedTestsEventSerializer extends Serializer[GeneratedTestsEvent] {
+class SpeculativeScreenPlaySerializer extends Serializer[SpeculativeScreenPlay] {
   override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {}
 
-  override def serialize(topic: String, data: GeneratedTestsEvent): Array[Byte] = {
-    val writer: DatumWriter[GeneratedTestsEvent] =
-      new SpecificDatumWriter[GeneratedTestsEvent](GeneratedTestsEvent.SCHEMA$)
+  override def serialize(topic: String, data: SpeculativeScreenPlay): Array[Byte] = {
+    val writer: DatumWriter[SpeculativeScreenPlay] =
+      new SpecificDatumWriter[SpeculativeScreenPlay](SpeculativeScreenPlay.SCHEMA$)
 
     val out = new ByteArrayOutputStream()
     val encoder = new EncoderFactory().binaryEncoder(out, null)
