76dc5988a8f6fb7ff2705a0f8f790ade8c65020d
Andy Boyle <andy@sudostream.io>
2017-08-31 12:02:26 +0100

    Added ScottishEsAndOs Avro definition

--- src/test/scala/io/sudostream/timetoteach/messages/ScottishEsAndOsTest.scala
+++ src/test/scala/io/sudostream/timetoteach/messages/ScottishEsAndOsTest.scala
@@ -1,5 +1,6 @@
 package io.sudostream.timetoteach.messages
 
+
 import org.scalatest.FunSuite
 
 class ScottishEsAndOsTest extends FunSuite {
@@ -14,18 +15,52 @@ class ScottishEsAndOsTest extends FunSuite {
       "the fourth, as far as is consistent with each child or young person’s needs. Further information on all " +
       "learner entitlements can be found in 'Building the Curriculum 3: A framework for learning and teaching'."
 
-    val introductionParagraphs = List(
+    val introductionParagraphs: List[EsAndOsIntroductionParagraph] = List(
       EsAndOsIntroductionParagraph("Introduction", introductionText),
       EsAndOsIntroductionParagraph("A broad general education", broadEducationText
       )
     )
 
-    val mapOfLevelsToStage = Map(
-      "EEARLY" -> "The pre-school years and P1, or later for some.",
+    val mapOfLevelsToStage: Map[String, String] = Map(
+      "EARLY" -> "The pre-school years and P1, or later for some.",
       "FIRST" -> "To the end of P4, but earlier or later for some.",
       "SECOND" -> "To the end of P7, but earlier or later for some.",
       "THIRD" -> "and Fourth S1 to S3, but earlier for some. The fourth level broadly equates to SCQF level 4.",
       "FOURTH" -> "phase S4 to S6, and college or other means of study"
+    )
+
+    //// Lets build a small example
+    val feelingsStatement = ExperienceAndOutcomeStatement(
+      eAndOText = "I am aware of and able to express my feelings and am developing the ability to talk about them.",
+      stage = List(
+        CurriculumCodeAndLevel(curriculumLevel = CurriculumLevel.EARLY, eAndOCode = "HWB 0-01a"),
+        CurriculumCodeAndLevel(curriculumLevel = CurriculumLevel.FIRST, eAndOCode = "HWB 1-01a"),
+        CurriculumCodeAndLevel(curriculumLevel = CurriculumLevel.SECOND, eAndOCode = "HWB 2-01a"),
+        CurriculumCodeAndLevel(curriculumLevel = CurriculumLevel.THIRD, eAndOCode = "HWB 3-01a"),
+        CurriculumCodeAndLevel(curriculumLevel = CurriculumLevel.FOURTH, eAndOCode = "HWB 4-01a")
+      )
+    )
+
+    val emotionStatement = ExperienceAndOutcomeStatement(
+      eAndOText = "I know that we all experience a variety of thoughts and emotions that affect" +
+        " how we feel and behave and I am learning ways of managing them.",
+      stage = List(
+        CurriculumCodeAndLevel(curriculumLevel = CurriculumLevel.EARLY, eAndOCode = "HWB 0-02a"),
+        CurriculumCodeAndLevel(curriculumLevel = CurriculumLevel.FIRST, eAndOCode = "HWB 1-02a"),
+        CurriculumCodeAndLevel(curriculumLevel = CurriculumLevel.SECOND, eAndOCode = "HWB 2-02a"),
+        CurriculumCodeAndLevel(curriculumLevel = CurriculumLevel.THIRD, eAndOCode = "HWB 3-02a"),
+        CurriculumCodeAndLevel(curriculumLevel = CurriculumLevel.FOURTH, eAndOCode = "HWB 4-02a")
+      )
+    )
+
+    val eAndOSetSubSection: EAndOSetSubSection = EAndOSetSubSection(
+      eAndOSetSubSectionName = "Mental and emotional wellbeing",
+      experienceAndOutcomeStatements = List(feelingsStatement, emotionStatement)
+    )
+
+    val theEAndOSetSection: EAndOSetSection = EAndOSetSection(
+      eAndOSetSectionName = "Mental, emotional, social and physical wellbeing",
+      eAndOSetSubSection = List(eAndOSetSubSection)
     )
 
     val curriculumAreas = List(
@@ -41,6 +76,12 @@ class ScottishEsAndOsTest extends FunSuite {
             "Learning in health and wellbeing ensures that children and young people develop the knowledge" +
               " and understanding, skills, capabilities and attributes which they need for mental, emotional," +
               " social and physical wellbeing now and in the future. ")
+        ),
+        eAndOSet = List(
+          EAndOSet(
+            eAndOSetName = "DEFAULT",
+            eAndOSetSection = List(theEAndOSetSection)
+          )
         )
       )
     )
@@ -52,9 +93,15 @@ class ScottishEsAndOsTest extends FunSuite {
     )
 
     assert(esAndOs.introduction.head.text == introductionText)
-    assert(esAndOs.mapCurriculumLevelsToStage("Second") == "To the end of P7, but earlier or later for some.")
+    assert(esAndOs.mapCurriculumLevelsToStage("SECOND") == "To the end of P7, but earlier or later for some.")
     assert(esAndOs.curriculumAreas.head.curriculumAreaName == "Health And Wellbeing")
     assert(esAndOs.curriculumAreas.head.principlesAndPractice.size == 2)
+
+    // Here we are just checking deep in the structure that the first statement is as expected.
+    assert(
+      esAndOs.curriculumAreas.head.eAndOSet.head.eAndOSetSection.
+        head.eAndOSetSubSection.head.experienceAndOutcomeStatements.head.eAndOText ==
+        "I am aware of and able to express my feelings and am developing the ability to talk about them.")
   }
 
 }
