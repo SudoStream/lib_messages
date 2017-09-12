e08e86c8df569f361104c49834eb9a66e2477ecf
Andy Boyle <andy@sudostream.io>
2017-09-12 11:37:06 +0100

    add GetScottishEsAndOsDataRequest (de)serialiser

---
+++ src/main/scala/io/sudostream/timetoteach/kafka/serializing/GetScottishEsAndOsDataRequestDeserializer.scala
@@ -0,0 +1,29 @@
+package io.sudostream.timetoteach.kafka.serializing
+
+import java.io.{ByteArrayInputStream, InputStream}
+import java.util
+
+import io.sudostream.timetoteach.messages.scottish.GetScottishEsAndOsDataRequest
+import org.apache.avro.io.DecoderFactory
+import org.apache.avro.specific.SpecificDatumReader
+import org.apache.kafka.common.serialization.Deserializer
+
+class GetScottishEsAndOsDataRequestDeserializer extends Deserializer[GetScottishEsAndOsDataRequest] {
+
+  override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {}
+
+  override def deserialize(topic: String, data: Array[Byte]): GetScottishEsAndOsDataRequest = {
+    val reader = new SpecificDatumReader[GetScottishEsAndOsDataRequest](GetScottishEsAndOsDataRequest.SCHEMA$)
+    val in: InputStream = new ByteArrayInputStream(data)
+    val decoder: org.apache.avro.io.Decoder = new DecoderFactory().binaryDecoder(in, null)
+    val outVersion = new GetScottishEsAndOsDataRequest()
+    reader.read(outVersion, decoder)
+    outVersion
+  }
+
+  override def close(): Unit = {}
+
+}
+
+
+}
---
+++ src/main/scala/io/sudostream/timetoteach/kafka/serializing/GetScottishEsAndOsDataRequestSerializer.scala
@@ -0,0 +1,32 @@
+package io.sudostream.timetoteach.kafka.serializing
+
+import java.io.ByteArrayOutputStream
+import java.util
+
+import io.sudostream.timetoteach.messages.scottish.GetScottishEsAndOsDataRequest
+import org.apache.avro.io.{DatumWriter, EncoderFactory}
+import org.apache.avro.specific.SpecificDatumWriter
+import org.apache.kafka.common.serialization.Serializer
+
+class GetScottishEsAndOsDataRequestSerializer extends Serializer[GetScottishEsAndOsDataRequest] {
+
+  override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {}
+
+  override def serialize(topic: String, data: GetScottishEsAndOsDataRequest): Array[Byte] = {
+    val writer: DatumWriter[GetScottishEsAndOsDataRequest] =
+      new SpecificDatumWriter[GetScottishEsAndOsDataRequest](GetScottishEsAndOsDataRequest.SCHEMA$)
+
+    val out = new ByteArrayOutputStream()
+    val encoder = new EncoderFactory().binaryEncoder(out, null)
+    writer.write(data, encoder)
+    encoder.flush()
+    new Array[Byte](1024)
+    out.toByteArray
+  }
+
+  override def close(): Unit = {}
+
+}
+
+
+}
