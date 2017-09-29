47a3a9c7fcaf5ed663e506ff11e8246ec3d147a7
Andy Boyle <andy@sudostream.io>
2017-09-29 13:44:31 +0100

    add schools (de)serialiser

--- src/main/scala/io/sudostream/timetoteach/kafka/serializing/systemwide/model/SchoolsSerializer.scala
+++ src/main/scala/io/sudostream/timetoteach/kafka/serializing/systemwide/model/SchoolsSerializer.scala
@@ -3,18 +3,18 @@ package io.sudostream.timetoteach.kafka.serializing.systemwide.model
 import java.io.ByteArrayOutputStream
 import java.util
 
-import io.sudostream.timetoteach.messages.systemwide.model.School
+import io.sudostream.timetoteach.messages.systemwide.model.Schools
 import org.apache.avro.io.{DatumWriter, EncoderFactory}
 import org.apache.avro.specific.SpecificDatumWriter
 import org.apache.kafka.common.serialization.Serializer
 
-class SchoolSerializer extends Serializer[School] {
+class SchoolsSerializer extends Serializer[Schools] {
 
   override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {}
 
-  override def serialize(topic: String, data: School): Array[Byte] = {
-    val writer: DatumWriter[School] =
-      new SpecificDatumWriter[School](School.SCHEMA$)
+  override def serialize(topic: String, data: Schools): Array[Byte] = {
+    val writer: DatumWriter[Schools] =
+      new SpecificDatumWriter[Schools](Schools.SCHEMA$)
 
     val out = new ByteArrayOutputStream()
     val encoder = new EncoderFactory().binaryEncoder(out, null)
