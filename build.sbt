a71ef2dec71c553d1bffd17d056053a2062b6594
Andy Boyle <andy@sudostream.io>
2018-02-06 16:14:17 +0000

    0.0.11-48 : Refactor ScottishEsAndOs

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := """messages"""
 organization := "io.sudostream.timetoteach"
-version := "0.0.11-47"
+version := "0.0.11-48"
 
 scalaVersion := "2.11.7"
 ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true)}
--- src/test/scala/io/sudostream/timetoteach/messages/scottish/ScottishEsAndOsDataTest.scala
+++ src/test/scala/io/sudostream/timetoteach/messages/scottish/ScottishEsAndOsDataTest.scala
@@ -1,68 +1,70 @@
 package io.sudostream.timetoteach.messages.scottish
 
-import io.sudostream.timetoteach.messages.systemwide.model.ScottishCurriculumLevelWrapper
 import org.scalatest.FunSuite
 
 class ScottishEsAndOsDataTest extends FunSuite {
 
   test("Basic ScottishEsAndOsDataTest") {
 
-    val experiencesAndOutcomes: List[ScottishEsAndOsMetadata] = List(
-      ScottishEsAndOsMetadata(
-        experienceAndOutcome = List(
-          ScottishExperienceAndOutcome(
-            sentence = "I have experienced the energy and excitement of presenting/performing for " +
-              "audiences and being part of an audience for other people’s presentations/performances.",
-            bulletPoints = List()
+    val experiencesAndOutcomes: List[ScottishEsAndOsBySubSection] = List(
+      ScottishEsAndOsBySubSection(
+        experienceAndOutcomes = List(
+          SingleScottishExperienceAndOutcome(
+            code = "EXA 0-01a",
+            eAndOLines = List(
+              ScottishExperienceAndOutcomeLine(
+                sentence = "I have experienced the energy and excitement of presenting/performing for " +
+                  "audiences and being part of an audience for other people’s presentations/performances.",
+                bulletPoints = Nil
+              )
+            )
           )
         ),
-        codes = List("EXA 0-01a", "EXA 1-01a", "EXA 2-01a"),
-        curriculumLevels = List(
-          ScottishCurriculumLevelWrapper(ScottishCurriculumLevel.EARLY),
-          ScottishCurriculumLevelWrapper(ScottishCurriculumLevel.FIRST),
-            ScottishCurriculumLevelWrapper(ScottishCurriculumLevel.SECOND)
-        ),
+        scottishCurriculumLevel = ScottishCurriculumLevel.EARLY,
+        associatedBenchmarks = List(),
         curriculumAreaName = ScottishCurriculumAreaName.EXPRESSIVE_ARTS,
-        eAndOSetName = Option.empty,
         eAndOSetSectionName = "Participation in performances and presentations",
         eAndOSetSubSectionName = Option.empty,
         eAndOSetSubSectionAuxiliaryText = Option.empty,
         responsibilityOfAllPractitioners = true
       ),
-      ScottishEsAndOsMetadata(
-        experienceAndOutcome = List(
-          ScottishExperienceAndOutcome(
-            sentence = "I have used the skills I have developed in the expressive arts to contribute to a " +
-              "public presentation/performance.",
-            bulletPoints = List()
+      ScottishEsAndOsBySubSection(
+        experienceAndOutcomes = List(
+          SingleScottishExperienceAndOutcome(
+            code = "EXA 1-01a",
+            eAndOLines = List(
+              ScottishExperienceAndOutcomeLine(
+                sentence = "I have experienced the energy and excitement of presenting/performing for " +
+                  "audiences and being part of an audience for other people’s presentations/performances.",
+                bulletPoints = Nil
+              )
+            )
           )
         ),
-        codes = List("EXA 3-01a"),
-        curriculumLevels = List(
-          ScottishCurriculumLevelWrapper(ScottishCurriculumLevel.THIRD)
-        ),
+        scottishCurriculumLevel = ScottishCurriculumLevel.FIRST,
+        associatedBenchmarks = List(),
         curriculumAreaName = ScottishCurriculumAreaName.EXPRESSIVE_ARTS,
-        eAndOSetName = Option.empty,
         eAndOSetSectionName = "Participation in performances and presentations",
         eAndOSetSubSectionName = Option.empty,
         eAndOSetSubSectionAuxiliaryText = Option.empty,
         responsibilityOfAllPractitioners = true
-      ),
-      ScottishEsAndOsMetadata(
-        experienceAndOutcome = List(
-          ScottishExperienceAndOutcome(
-            sentence = "In everyday activity and play, I explore and make choices to develop my learning and interests. " +
-              "I am encouraged to use and share my experiences.",
-            bulletPoints = List()
+      ),ScottishEsAndOsBySubSection(
+        experienceAndOutcomes = List(
+          SingleScottishExperienceAndOutcome(
+            code = "EXA 2-01a",
+            eAndOLines = List(
+              ScottishExperienceAndOutcomeLine(
+                sentence = "I have experienced the energy and excitement of presenting/performing for " +
+                  "audiences and being part of an audience for other people’s presentations/performances.",
+                bulletPoints = Nil
+              )
+            )
           )
         ),
-        codes = List("HWB 0-19a"),
-        curriculumLevels = List(
-          ScottishCurriculumLevelWrapper(ScottishCurriculumLevel.EARLY)
-        ),
-        curriculumAreaName = ScottishCurriculumAreaName.HEALTH_AND_WELLBEING,
-        eAndOSetName = Option.empty,
-        eAndOSetSectionName = "Planning for choices and changes",
+        scottishCurriculumLevel = ScottishCurriculumLevel.SECOND,
+        associatedBenchmarks = List(),
+        curriculumAreaName = ScottishCurriculumAreaName.EXPRESSIVE_ARTS,
+        eAndOSetSectionName = "Participation in performances and presentations",
         eAndOSetSubSectionName = Option.empty,
         eAndOSetSubSectionAuxiliaryText = Option.empty,
         responsibilityOfAllPractitioners = true
@@ -76,8 +78,8 @@ class ScottishEsAndOsDataTest extends FunSuite {
     assert(esAndOsData.allExperiencesAndOutcomes.size == 3)
 
     val eAndOsSatisfyingFilter = esAndOsData.allExperiencesAndOutcomes.filter(
-      (metadata) => metadata.eAndOSetSectionName == "Planning for choices and changes")
-    assert(eAndOsSatisfyingFilter.size == 1)
+      (metadata) => metadata.eAndOSetSectionName == "Participation in performances and presentations")
+    assert(eAndOsSatisfyingFilter.size == 3)
 
     println(esAndOsData.toString)
 
